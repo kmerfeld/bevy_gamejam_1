1138165080537242593dc4204dd860240e50ca2e
Sean Warne <Sean-Warne@users.noreply.github.com>
2022-02-27 16:44:20 -0700

    Rocks and collision (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use bevy::core::FixedTimestep;
 use bevy::math::const_vec2;
 use bevy::prelude::*;
-//use rand::prelude::random;
+use bevy::sprite::collide_aabb::collide;
+use rand::Rng;
 
 const TIME_STEP: f32 = 0.1;
 
-const WINDOW_HEIGHT: f32 = 1000.0;
-const WINDOW_WIDTH: f32 = 1000.0;
+const WINDOW_HEIGHT: f32 = 750.0;
+const WINDOW_WIDTH: f32 = 750.0;
 const BOUNDS: Vec2 = const_vec2!([WINDOW_HEIGHT, WINDOW_WIDTH]);
 
-const ARENA_WIDTH: u32 = 200;
-const ARENA_HEIGHT: u32 = 200;
+const ARENA_WIDTH: u32 = 300;
+const ARENA_HEIGHT: u32 = 300;
 
 const FORWARD_MOVE_DIST: f32 = 50.0;
 
@@ -27,10 +28,13 @@ fn main() {
         .add_system_set_to_stage(
             CoreStage::PostUpdate,
             SystemSet::new()
-                // .with_system(position_translation)
+                .with_system(position_translation)
                 .with_system(size_scaling),
         )
-        .add_startup_system(setup)
+        .add_startup_system(setup_camera)
+        .add_startup_system(setup_rocks)
+        .add_startup_system(spawn_player_ship)
+        .add_startup_system(spawn_enemy_ships)
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
@@ -81,34 +85,94 @@ struct Player1;
 #[derive(Component, Debug, Clone, Copy, PartialEq)]
 struct Player2;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let player_ship = asset_server.load("textures/ships/ship (8).png");
-    let enemy_ship = asset_server.load("textures/ships/ship (10).png");
-    // let water_bkg = asset_server.load("assets/textures/tiles/tile_73.png");
-    // let font = asset_server.load("fonts/FiraMono-Regular.ttf");
+struct GameOverEvent;
 
+fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn setup_rocks(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let rocks: [Handle<Image>; 3] = [asset_server.load("textures/tiles/tile_49.png"),
+                                     asset_server.load("textures/tiles/tile_50.png"),
+                                     asset_server.load("textures/tiles/tile_51.png")];
+
+    let mut spawned_rocks = vec![];
+
+    for i in 0..3 {
+        let rock_type: usize = rand::thread_rng().gen_range(0, rocks.len());
+        let mut rock_x: f32 = rand::thread_rng().gen_range(60.0, (ARENA_WIDTH as f32) - 50.0);
+        let mut rock_y: f32 = rand::thread_rng().gen_range(60.0, (ARENA_HEIGHT as f32) - 60.0);
+        let rock_rot: f32 = rand::thread_rng().gen_range(0.0, 360.0);
+        let rock_size: f32 = rand::thread_rng().gen_range(0.3, 1.1);
+
+        // make sure rocks are spaced apart
+        if spawned_rocks.len() > 0 {
+            for j in 0..spawned_rocks.len() {
+                let spawned_tmp: (f32, f32) = spawned_rocks[j];
+                let spawned_x: f32 = spawned_tmp.0;
+                let spawned_y: f32 = spawned_tmp.1;
+
+                while (rock_x >= spawned_x - 50.0 && rock_x <= spawned_x + 50.0) && 
+                      (rock_y >= spawned_y - 50.0 && rock_y <= spawned_x + 50.0) {
+                    rock_x = rand::thread_rng().gen_range(60.0, (ARENA_WIDTH as f32) - 50.0);
+                    rock_y = rand::thread_rng().gen_range(60.0, (ARENA_HEIGHT as f32) - 60.0);
+                }
+            }
+            spawned_rocks.push((rock_x, rock_y));
+        } else {
+            spawned_rocks.push((rock_x, rock_y));
+        }
+
+        // println!("Spaned rocks: {:?}", spawned_rocks);
+
+        commands
+            .spawn_bundle(SpriteBundle {
+                texture: rocks[rock_type].clone(),
+                transform: Transform {
+                    scale: Vec3::new(10.0, 10.0, 10.0),
+                    rotation: Quat::from_rotation_z(f32::to_radians(rock_rot)),
+                    ..Default::default()
+                },
+                ..Default::default()
+        })
+        .insert(Position {
+            x: rock_x as i32,
+            y: rock_y as i32,
+        })
+        .insert(Size::square(rock_size));
+    }
+}
+
+fn spawn_player_ship(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let player_ship = asset_server.load("textures/ships/ship (10).png");
+    // let ship_rot: i32 = rand::thread_rng().gen_range(-90, 1);
 
     commands
         .spawn_bundle(SpriteBundle {
             texture: player_ship,
             transform: Transform {
                 scale: Vec3::new(10.0, 10.0, 10.0),
-                translation: Vec3::new(100.0, 0.0, 0.0),
+                translation: Vec3::new((ARENA_WIDTH as f32) - 30.0, -(ARENA_HEIGHT as f32) + 30.0, 0.0),
+                // rotation: Quat::from_rotation_z(f32::to_radians(ship_rot)),
                 ..Default::default()
             },
             ..Default::default()
         })
         .insert(Player)
         .insert(PlayerTurn(Turn::Player1))
-        .insert(Size::square(0.15));
+        .insert(Size::square(0.3));    
+}
+
+fn spawn_enemy_ships(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let enemy_ship = asset_server.load("textures/ships/ship (8).png");
+    // let ship_rot: i32 = rand::thread_rng().gen_range(90, 181);
 
     commands
         .spawn_bundle(SpriteBundle {
             texture: enemy_ship,
             transform: Transform {
                 scale: Vec3::new(10.0, 10.0, 10.0),
-                translation: Vec3::new(-100.0, 0.0, 0.0),
+                translation: Vec3::new(-(ARENA_WIDTH as f32) + 30.0, (ARENA_HEIGHT as f32) - 30.0, 0.0),
                 rotation: Quat::from_rotation_z(f32::to_radians(180.0)),
                 ..Default::default()
             },
@@ -122,7 +186,8 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn ship_movement(
     mut player_turn: ResMut<PlayerTurn>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut player_q: Query<(With<Player>, &mut Transform, &PlayerTurn)>,
+    mut player_q: Query<(&Player, &mut Transform, &PlayerTurn)>,
+    ship_positions: Query<&mut Position, With<Player>>,
 ) {
     for (_, mut transform, player) in player_q.iter_mut() {
         if player.0 == player_turn.0 {
@@ -186,5 +251,29 @@ fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
             sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
             1.0,
         );
+    }
+}
+
+fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
+    }
+}
+
+fn game_over(
+    mut commands: Commands,
+    mut reader: EventReader<GameOverEvent>,
+) {
+    if reader.iter().next().is_some() {
+        // if you hit a rock, hit the enemy ship, get killed by the enemy, or kill the enemy - respawn
     }
 }
