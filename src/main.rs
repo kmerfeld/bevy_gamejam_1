1c0a1653322b518521c5e056f80fefba21697f71
kyle merfeld <kmerfeld1@gmail.com>
2022-02-27 19:23:26 -0500

    Disable bad position data

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() {
         .add_system_set_to_stage(
             CoreStage::PostUpdate,
             SystemSet::new()
-                .with_system(position_translation)
+                //.with_system(position_translation)
                 .with_system(size_scaling),
         )
         .add_startup_system(setup_camera)
@@ -331,21 +331,6 @@ fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
             sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
             sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
             1.0,
-        );
-    }
-}
-
-fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
-    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
-        let tile_size = bound_window / bound_game;
-        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
-    }
-    let window = windows.get_primary().unwrap();
-    for (pos, mut transform) in q.iter_mut() {
-        transform.translation = Vec3::new(
-            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
-            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
-            0.0,
         );
     }
 }
